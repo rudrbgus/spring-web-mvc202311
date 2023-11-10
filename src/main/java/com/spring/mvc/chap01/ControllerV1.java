158b308a6532f1c7e8e6084491bd3113250ea8d0
rudrbgus <rudrbgus3@gmail.com>
2023-11-10 11:09:22 +0900

    #1. 스프링 요청 처리 및 jsp 연결

---
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -0,0 +1,60 @@
+package com.spring.mvc.chap01;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+// 컨트롤러: 클라이언트의 요청을 받아서 처리 후 응답을 보내주는 역할
+//@Component // 빈 등록 : 이 클래스의 객체 생성 관리는 스프링이 처리한다.
+@Controller
+public class ControllerV1 {
+    // 세부요청처리는 메서드를 통해 등록
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("Welcome to my webpage !!");
+
+        // 리턴문에는 어떤 jsp로 포워딩할지 경로를 적습니다.
+        return "index";
+    }
+
+    // /food 요청이 오면 food.jsp파일을 열어보세요
+    @RequestMapping("/food")
+    public String food(){
+        return "chap01/food";
+    }
+
+    // ========= 요청 파라미터 읽기 (클라이언트가 보낸 정보) ======= //
+
+    // 1. HttpServletRequest 객체 이용하기
+    // ex) /person?name=kim&age=30
+    @RequestMapping("/person")
+    public String person(HttpServletRequest req){
+        String name = req.getParameter("name");
+        String age = req.getParameter("age");
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        return "";
+    }
+
+    // 2. @RequestParam 사용하기
+    // ex : /major?stu=park&major=business&grade=3
+    @RequestMapping("/major")
+    public String major(String stu,
+                        @RequestParam("major") String mj,
+                        @RequestParam(defaultValue = "0") Integer grade ){
+        System.out.println("stu = " + stu);
+        System.out.println("major = " + mj);
+        System.out.println("grade = " + grade);
+        return "";
+    }
+
+    // 3. DTO(Data Transfer Object)객체 사용하기
+    // -> 파라미터의 양이 엄청 많거나 서로 연관되어 있는 경우에 사용
+    // ex) /order/orderNum=123&goodsName=구두&amount=3&price=200000....
+
+
+}
